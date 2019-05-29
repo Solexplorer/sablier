49a3bd06f9c53335ba8638714f8309a07c3110f2
Paul Berg <hello@paulrberg.com>
2019-05-30 00:39:43 +0100

    fix(frontend): trojan horse :)

--- packages/frontend/README.md
+++ packages/frontend/README.md
@@ -1,5 +1,3 @@
-## Sablier Frontend
-
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,31 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ReactGA from "react-ga";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import { ApolloProvider } from "react-apollo";
-import { Provider } from "react-redux";
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import App from "./App";
-import client from "./apollo/client";
-import store from "./redux/store";
-
-import "./i18n";
-import "sanitize.css/sanitize.css";
-
-import "./styles/index.scss";
-
-if (process.env.NODE_ENV === "production") {
-  ReactGA.initialize("UA-107325747-7");
-} else {
-  ReactGA.initialize("test", { testMode: true });
-}
-ReactGA.pageview(window.location.pathname + window.location.search);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </Provider>,
-  document.querySelector("#root"),
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
